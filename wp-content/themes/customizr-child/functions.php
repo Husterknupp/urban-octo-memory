91f98f6e628929819f245a90aafcadb8686fcc11
benjamin <husterknupp@outlook.com>
2019-06-23 20:27:45 +0200

    modified theme `Customizr Theme Child` version 1.0

--- wp-content/themes/customizr-child/functions.php
+++ wp-content/themes/customizr-child/functions.php
@@ -4,7 +4,7 @@
  * */
 function child_theme_styles() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'child-theme-css', get_stylesheet_directory_uri() .'/style.css' , array('parent-style'));
+	wp_enqueue_style( 'child-theme-style', get_stylesheet_directory_uri() .'/style.css' , array('parent-style'));
 
 }
 add_action( 'wp_enqueue_scripts', 'child_theme_styles' );
@@ -14,7 +14,7 @@ add_action( 'wp_enqueue_scripts', 'child_theme_styles' );
  * */
 function theme_slug_scripts() {
 	$modificated = date( 'YmdHi', filemtime( get_stylesheet_directory() . '/style.css' ) );
-	wp_enqueue_style( 'theme-slug-stylesheet', get_stylesheet_uri(), array(), $modificated );
+	wp_enqueue_style( 'child-theme-style', get_stylesheet_uri(), array(), $modificated );
 }
 add_action( 'wp_enqueue_scripts', 'theme_slug_scripts' );
 
