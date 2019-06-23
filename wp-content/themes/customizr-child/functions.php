87a6dbfd9f5912f9dd23053300d495da5a9ee6ab
benjamin <husterknupp@outlook.com>
2019-06-23 20:56:02 +0200

    modified theme `Customizr Theme Child` version 1.0

--- wp-content/themes/customizr-child/functions.php
+++ wp-content/themes/customizr-child/functions.php
@@ -2,15 +2,15 @@
 /**
  * * include child theme stylesheet after the original stylesheet
  * */
-// function child_theme_styles() {
-// 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+function child_theme_styles() {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 // 	wp_enqueue_style( 'child-theme-style', get_stylesheet_directory_uri() .'/style.css' , array('parent-style'));
 
 // 	 for caching we need dynamic css url version number
-// 	$modificated = date( 'YmdHi', filemtime( get_stylesheet_directory() . '/style.css' ) );
-// 	wp_enqueue_style( 'child-theme-style', get_stylesheet_uri(), array(), $modificated );
-// }
-// add_action( 'wp_enqueue_scripts', 'child_theme_styles' );
+	$modificated = date( 'YmdHi', filemtime( get_stylesheet_directory() . '/style.css' ) );
+	wp_enqueue_style( 'child-theme-style', get_stylesheet_uri(), array(), $modificated );
+}
+add_action( 'wp_enqueue_scripts', 'child_theme_styles' );
 
 /* 
  * DISABLE WORDPRESS ADMIN BAR FOR ALL USERS BUT ADMINS
