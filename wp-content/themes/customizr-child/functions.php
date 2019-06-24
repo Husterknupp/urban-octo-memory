a0ac6ffe6fe73fde46cbf1a4211d5409f67bb98e
benjamin <husterknupp@outlook.com>
2019-06-24 13:13:36 +0200

    modified theme `Customizr Pro Theme Child` version 1.0

--- wp-content/themes/customizr-child/functions.php
+++ wp-content/themes/customizr-child/functions.php
@@ -3,6 +3,9 @@
  * * include child theme stylesheet after the original stylesheet
  * */
 function child_theme_styles() {
+	wp_deregister_style( 'customizr-style' ); 
+	wp_deregister_style( 'customizr-style-css' ); 
+	
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 // 	wp_enqueue_style( 'child-theme-style', get_stylesheet_directory_uri() .'/style.css' , array('parent-style'));
 
