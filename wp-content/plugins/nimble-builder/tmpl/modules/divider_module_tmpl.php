37ebbe50338d91ff221a5c6810bec03fd48a782a
benjamin <husterknupp@outlook.com>
2019-09-06 21:03:32 +0200

    deleted plugin `Nimble Page Builder` version 1.8.20

--- wp-content/plugins/nimble-builder/tmpl/modules/divider_module_tmpl.php
+++
@@ -1,6 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) {
-    exit;
-}
-
-echo '<span class="sek-divider"></span>';
--- wp-content/plugins/nimble-builder/tmpl/modules/spacer_module_tmpl.php
+++
@@ -1,6 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) {
-    exit;
-}
-
-echo '<div class="sek-spacer"></div>';
